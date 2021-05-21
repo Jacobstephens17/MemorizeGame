db4463d8fa915cb4e782445bc02b1af727a97254
Jacob Stephens <jacobstephens2@gmail.com>
2021-05-21 16:48:35 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jacob Stephens on 5/21/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
